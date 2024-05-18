a8ff1e580a2f6320c41181752fe00b8a1c015a3b
suhiaing <suhlaingthin288@gmail.com>
2024-05-18 14:06:19 +0630

    future added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:habbit_tracker_flutter/constants.dart';
+import 'package:habbit_tracker_flutter/pages/home.dart';
 import 'package:habbit_tracker_flutter/pages/particular_data_table.dart';
 import 'package:habbit_tracker_flutter/providers/main_provider.dart';
 import 'package:habbit_tracker_flutter/providers/special_checkbox_provider.dart';
@@ -9,7 +10,6 @@ void main() {
   runApp(
     MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: ((context) => MainPVDS())),
         ChangeNotifierProvider(create: ((context) => SpecialCheckBoxPVD()))
       ],
       child: const MyApp(),
@@ -25,15 +25,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(
           scaffoldBackgroundColor: const Color.fromRGBO(244, 237, 202, 1)),
-      home: Scaffold(
-        body: Consumer<MainPVDS>(
-          builder: (context, mainPVDS, _) {
-            List<dynamic> firstDate = constants[0]["data"];
-            int lengthOfDate = (firstDate.length) + 1;
-            return ParticularDataTable(
-                lengthOfDate: lengthOfDate, firstDate: firstDate);
-          },
-        ),
+      home: const Scaffold(
+        body: Home(),
       ),
     );
   }
---
+++ lib/pages/home.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:habbit_tracker_flutter/constants.dart';
+import 'package:habbit_tracker_flutter/data/read_file.dart';
+import 'package:habbit_tracker_flutter/pages/particular_data_table.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  late Future<List<dynamic>> constantF;
+  @override
+  void initState() {
+    super.initState();
+    constantF = readFile("data.json");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: constantF,
+        builder: (context, snapshot) {
+          constants = snapshot.data!;
+
+          List<dynamic> firstDate = constants[0]["data"];
+          int lengthOfDate = (firstDate.length) + 1;
+          return ParticularDataTable(
+              lengthOfDate: lengthOfDate, firstDate: firstDate);
+        });
+  }
+}
